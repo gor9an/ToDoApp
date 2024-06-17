11ac8cbc73297512274e77f2993eb1ae9addbbf1
gor9an <gor9an@mail.ru>
2024-06-17 21:03:23 +0300

    implement TodoItem

--- ToDoApp/Models/TodoItem.swift
+++ ToDoApp/Models/TodoItem.swift
@@ -8,7 +8,42 @@
 import Foundation
 
 struct TodoItem {
+    let id: String?
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let dateOfCreation: Date
+    let dateOfChange: Date?
     
+    init(
+        id: String?,
+        text: String,
+        importance: Importance,
+        deadline: Date?,
+        isDone: Bool,
+        dateOfCreation: Date,
+        dateOfChange: Date?
+    ) {
+        if let id {
+            self.id = id
+        } else {
+            self.id = UUID().uuidString
+        }
+        
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isDone = isDone
+        self.dateOfCreation = dateOfCreation
+        self.dateOfChange = dateOfChange
+    }
+    
+    enum Importance {
+        case unimportant
+        case usual
+        case important
+    }
 }
 
 //extension TodoItem {
