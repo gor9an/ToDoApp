60e8bfc7dd14862dcd12a87bfb0b54ee0776e1a2
gor9an <gor9an@mail.ru>
2024-06-17 21:02:44 +0300

    add ToDoItem.swift, FileCache.swift; change the project structure

---
+++ ToDoApp/Models/TodoItem.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItem.swift
+//  ToDoApp
+//
+//  Created by Andrey Gordienko on 17.06.2024.
+//
+
+import Foundation
+
+struct TodoItem {
+    
+}
+
+//extension TodoItem {
+//    static func parse(json: Any) -> TodoItem? {
+//        
+//    }
+//}
---
+++ ToDoApp/Service/FileCache.swift
@@ -0,0 +1,12 @@
+//
+//  FileCache.swift
+//  ToDoApp
+//
+//  Created by Andrey Gordienko on 17.06.2024.
+//
+
+import Foundation
+
+final class FileCache {
+    
+}
