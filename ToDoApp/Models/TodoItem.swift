d1c5a7c26ed32f0afc7be6f0a73d6fe4bd179365
gor9an <gor9an@mail.ru>
2024-06-22 02:59:37 +0700

    TodoItem: add constants for convenience; replace [String: String] with [String: Any]

--- ToDoApp/Models/TodoItem.swift
+++ ToDoApp/Models/TodoItem.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-struct TodoItem {
+struct TodoItem: Equatable {
     // MARK: Properties
-    let id: String?
+    let id: String
     let text: String
     let importance: Importance
     let deadline: Date?
@@ -42,7 +42,7 @@ struct TodoItem {
     }
     
     // MARK: Importance enum
-    enum Importance {
+    enum Importance: String, Equatable {
         case unimportant
         case usual
         case important
@@ -51,55 +51,72 @@ struct TodoItem {
 
 // MARK: TodoItem extensions
 extension TodoItem {
+    struct Constants {
+        let idString = "id"
+        let textString = "text"
+        let importanceString = "importance"
+        let deadlineString = "deadline"
+        let isDoneString = "isDone"
+        let dateOfCreationString = "dateOfCreation"
+        let dateOfChangeString = "dateOfChange"
+    }
+    
     var json: Any {
         let formatter = JsonDateFormatter.standard
+        let const = Constants()
         
-        var todoItem = [
-            "id": "\(id!)",
-            "text": "\(text)",
-            "isDone": "\(isDone)",
-            "dateOfCreation": "\(formatter.string(from: dateOfCreation))"
-        ] as [String : String]
+        var todoItem: [String : Any] = [
+            const.idString: id,
+            const.textString: text,
+            const.isDoneString: isDone,
+            const.dateOfCreationString: "\(formatter.string(from: dateOfCreation))"
+        ]
         
-        if importance != .usual { todoItem["importance"] = "\(importance)" }
-        if let deadline { todoItem["deadline"] = formatter.string(from: deadline) }
-        if let dateOfChange { todoItem["dateOfChange"] = formatter.string(from: dateOfChange) }
+        if importance != .usual { todoItem[const.importanceString] = importance.rawValue }
+        if let deadline { todoItem[const.deadlineString] = formatter.string(from: deadline) }
+        if let dateOfChange { todoItem[const.dateOfChangeString] = formatter.string(from: dateOfChange) }
         
         return todoItem
     }
     
     static func parse(json: Any) -> TodoItem? {
-        guard let dictionary = json as? Dictionary<String, String> else { return nil }
-        
+        let const = Constants()
         let formatter = JsonDateFormatter.standard
+        guard let dictionary = json as? Dictionary<String, Any>,
+              let id = dictionary[const.idString] as? String,
+              let text = dictionary[const.textString] as? String,
+              let isDone = dictionary[const.isDoneString] as? Bool,
+              let creationDateString = dictionary[const.dateOfCreationString] as? String,
+              let dateOfCreation = formatter.date(from: creationDateString)
+        else { return nil }
+        
         var importance: Importance = .usual
         var deadline: Date? = nil
-        var dateOfCreation: Date? = nil
         var dateOfChange: Date? = nil
         
-        if let importanceString = dictionary["importance"] {
-            importance = importanceString ==  "important" ? Importance.important : Importance.unimportant
+        if let importanceString = dictionary[const.importanceString] as? String,
+           let importanceRaw = Importance(rawValue: importanceString)
+        {
+            importance = importanceRaw
+        } else {
+            importance = .usual
         }
         
-        if let deadlineString = dictionary["deadline"] {
+        if let deadlineString = dictionary["deadline"] as? String {
             deadline = formatter.date(from: deadlineString)
         }
         
-        if let creationDateString = dictionary["dateOfCreation"] {
-            dateOfCreation = formatter.date(from: creationDateString)
-        }
-        
-        if let changeDateString = dictionary["dateOfChange"] {
+        if let changeDateString = dictionary["dateOfChange"] as? String {
             dateOfChange = formatter.date(from: changeDateString)
         }
         
         let todoItem = TodoItem(
-            id: dictionary["id"],
-            text: dictionary["text"] ?? "",
+            id: id,
+            text: text,
             importance: importance,
             deadline: deadline,
-            isDone: dictionary["isDone"] == "true",
-            dateOfCreation: dateOfCreation ?? Date(),
+            isDone: isDone,
+            dateOfCreation: dateOfCreation,
             dateOfChange: dateOfChange
         )
         
