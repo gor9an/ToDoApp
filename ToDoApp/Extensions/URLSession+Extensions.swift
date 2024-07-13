149f08bef29445ecdc9d3fdb2d2c661a068fa549
gor9an <gor9an@mail.ru>
2024-07-13 04:41:16 +0300

    add URLSession: func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse)

--- ToDoApp/Extensions/URLSession+Extensions.swift
+++ ToDoApp/Extensions/URLSession+Extensions.swift
@@ -8,9 +8,36 @@
 import Foundation
 
 extension URLSession {
-//    let urlSession = URLSession.shared
-//    func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-//        urlSession.dataTask(with: <#T##URLRequest#>)
-//
-//    }
+    func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        let cancellableStorage = CancellableStorage()
+
+        return try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation { continuation in
+                let task = self.dataTask(with: urlRequest) { data, response, _ in
+
+                    if Task.isCancelled {
+                        continuation.resume(throwing: CancellationError())
+                        return
+                    }
+
+                    guard let data = data, let response = response else {
+                        return
+                    }
+
+                    continuation.resume(returning: (data, response))
+                }
+
+                task.resume()
+
+                cancellableStorage.add(task as ICancellable)
+
+                if Task.isCancelled {
+                    task.cancel()
+                    continuation.resume(throwing: CancellationError())
+                }
+            }
+        } onCancel: {
+            cancellableStorage.cancel()
+        }
+    }
 }
